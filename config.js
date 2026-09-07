c76de68adfd567d7e199c955dc90738313e1dcec
sholikkin5-afk <sholikkin5@gmail.com>
2026-09-07 16:42:39 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+// ====== CONFIG SARAHMART ======
+// Ganti kene tok nek arep ganti token/URL
+const CONFIG = {
+  SUPABASE_URL: 'https://fbxfoorzeojsqnzcmvic.supabase.co',
+  SUPABASE_ANON_KEY: 'sb_publishable_Xg3DLbSsRFTsU5ZjxIylkA_JP6pUpSK',
+  FONNTE_TOKEN: 'sWmuJHZWzniKqQ4AkUyv' // <-- GANTI TOKEN FONNTE KENE
+};
